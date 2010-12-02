85ebd331bdf273fbe957f2a9f42a7c1ee139e224
Bjørn Høyer <bjorn@bernsten.dk>
2010-12-02 22:11:47 +0100

    added plugins

---
+++ create_submodules.sh
@@ -0,0 +1,28 @@
+
+
+
+git submodule add git://github.com/astashov/vim-ruby-debugger.git bundle/vim-ruby
+git submodule add git://github.com/msanders/snipmate.vim.git bundle/snipmate
+git submodule add git://github.com/scrooloose/nerdtree.git bundle/nerdtree
+git submodule add git://github.com/scrooloose/nerdicommenter.git bundle/nerdcommenter
+git submodule add git://github.com/timcharper/textile.vim.git bundle/textile
+git submodule add git://github.com/wincent/Command-T.git bundle/Command-T
+git submodule add git://github.com/Townk/vim-autoclose.git bundle/autoclose
+git submodule add git://github.com/mexpolk/vim-taglist.git bundle/vim-taglist
+git submodule add git://github.com/tpope/vim-cucumber.git bundle/vim-cucumber
+git submodule add git://github.com/tpope/vim-fugitive.git bundle/fugitive
+git submodule add git://github.com/tpope/vim-git.git bundle/vim-git
+git submodule add git://github.com/tpope/vim-haml.git bundle/haml
+git submodule add git://github.com/tpope/vim-markdown.git bundle/markdown
+git submodule add git://github.com/tpope/vim-rails.git bundle/vim-rails
+git submodule add git://github.com/tpope/vim-repeat.git bundle/vim-repeat
+git submodule add git://github.com/tpope/vim-surround.git bundle/vim-surround
+git submodule add git://github.com/tpope/vim-vividchalk.git bundle/vividchalk
+git submodule add git://github.com/tsaleh/vim-align.git bundle/vim-align
+git submodule add git://github.com/tsaleh/vim-shoulda.git bundle/vim-shoulda
+git submodule add git://github.com/tsaleh/vim-supertab.git bundle/vim-supertab
+#git submodule add  # git://github.com/tsaleh/vim-tcomment.git
+git submodule add git://github.com/vim-ruby/vim-ruby.git bundle/vim-ruby
+
+
+
